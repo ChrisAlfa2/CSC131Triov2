5857d261bd41973b299d100c7cb3d54dca35c1a8
csusrassio <horacioambriz@csus.edu>
2024-11-28 13:00:36 -0800

    Update Hello.java

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("60785878 - This line was added by Christian Alfaro.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("302929640 - This line was added by Horacio Ambriz.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
