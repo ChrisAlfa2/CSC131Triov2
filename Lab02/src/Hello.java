5d2c0c72f868eb808729d4945cc346c6d0b00abc
ChrisAlfa2 <calfaro3@csus.edu>
2024-11-28 20:56:05 -0800

    Update Hello.java

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 		System.out.println("Welcome to Git!");
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
-		System.out.println("60785878 - This line was added by Christian Alfaro.");
+		System.out.println("302911297 - This line was added by Christian Alfaro.");
 		System.out.println("302929640 - This line was added by Horacio Ambriz.");
 		System.out.println("SacStateID - This line was added by Nelson San.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
